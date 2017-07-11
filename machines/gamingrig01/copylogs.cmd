07ba8e0402682976c49e7ae77e05508b025295af
Karl Flores <Karl Flores>
2017-07-11 11:31:55 -0700

    Change OBS reference paths for StreamLabels text files, archive Trials Fusion post stream changes.

--- machines/gamingrig01/copylogs.cmd
+++ machines/gamingrig01/copylogs.cmd
@@ -1,6 +1,6 @@
 set AppData_OBS-Studio=C:\Users\kutlass\AppData\Roaming\obs-studio
 set Source=%AppData_OBS-Studio%
-set Destination=C:\Projects\kutlassTwitch\machines\gamingrig01\obs-studio-fag
+set Destination=C:\Projects\kutlassTwitch\machines\gamingrig01\obs-studio
 
 xcopy %Source%\*.* %Destination% /ickherqy
 
