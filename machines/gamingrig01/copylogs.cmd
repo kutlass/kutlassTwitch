af0d67e3eeb0d224208d68a49344d1af8b13e381
Karl Flores <Karl Flores>
2017-07-10 22:29:50 -0700

    Add copylogs.cmd script for archiving OBS-Studio appdata folder

---
+++ machines/gamingrig01/copylogs.cmd
@@ -0,0 +1,7 @@
+set AppData_OBS-Studio=C:\Users\kutlass\AppData\Roaming\obs-studio
+set Source=%AppData_OBS-Studio%
+set Destination=C:\Projects\kutlassTwitch\machines\gamingrig01\obs-studio-fag
+
+xcopy %Source%\*.* %Destination% /ickherqy
+
+pause
